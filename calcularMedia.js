5e822defb2ed897444cd9caaa62ee2ff675f21d4
Raquel Oliveira Ferreira <rraquelof@gmail.com>
2022-06-02 15:58:57 -0300

    adicionando função de calcular média

---
+++ calcularMedia.js
@@ -0,0 +1,12 @@
+function CalcularMedia() {
+  var nota1 = parseFloat(document.getElementById('nota1').value)
+  var nota2 = parseFloat(document.getElementById('nota2').value)
+  var nota3 = parseFloat(document.getElementById('nota3').value)
+
+  var media = ((nota1 + nota2 + nota3) / 3).toFixed(1)
+
+  if (media >= 7)
+    document.getElementById('resultado').innerHTML = 'Aprovado! Média ' + media
+  else
+    document.getElementById('resultado').innerHTML = 'Reprovado! Média ' + media
+}
